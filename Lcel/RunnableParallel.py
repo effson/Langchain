44f57bf6ec1a42fbe7a2805b5fd492b2de1def0c
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-11 17:29:08 +0800

    Update RunnableParallel.py

--- Lcel/RunnableParallel.py
+++ Lcel/RunnableParallel.py
@@ -10,12 +10,14 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableParallel
 from loguru import logger
 import os
+from dotenv import load_dotenv
 
+load_dotenv()
 model = init_chat_model(
-    model="qwen-plus",
-    model_provider="openai",
-    api_key=os.getenv("aliQwen-api"),
-    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
+    model="deepseek-chat", # deepseek-chat 对应 DeepSeek-V3.2 的非思考模式
+    model_provider="deepseek",
+    api_key=os.getenv("deepseek-api"),
+    base_url='https://api.deepseek.com'
 )
 
 # 并行链1提示词
@@ -49,6 +51,37 @@ parallel_chain = RunnableParallel({
 result = parallel_chain.invoke({"topic": "langchain"})
 logger.info(result)
 
-
 # 打印并行链的ASCII图形表示，LangGraph提前预告，不是本节知识点
 parallel_chain.get_graph().print_ascii()
+
+
+"""
+2026-04-11 17:27:44.823 | INFO     | __main__:<module>:52 - {'chinese': 'LangChain是一个用于开发由大型语言模型驱动的应用程序的框架。它通过提供模块化组件和链式调用，简化了将LLM与外部数据源、工具和记忆系统集成的过程，从而构建更强大、可定制的AI应用。', 'english': 'LangChain is a framework for developing applications powered by language models. It simplifies the process of chaining together different components (like prompts, models, and data sources) to create complex, context-aware applications such as chatbots, agents, and retrieval-augmented generation (RAG) systems.'}
+             +--------------------------------+              
+             | Parallel<chinese,english>Input |              
+             +--------------------------------+              
+                   ****               ****                   
+                ***                       ***                
+              **                             **              
++--------------------+                +--------------------+ 
+| ChatPromptTemplate |                | ChatPromptTemplate | 
++--------------------+                +--------------------+ 
+           *                                     *           
+           *                                     *           
+           *                                     *           
+   +--------------+                      +--------------+    
+   | ChatDeepSeek |                      | ChatDeepSeek |    
+   +--------------+                      +--------------+    
+           *                                     *           
+           *                                     *           
+           *                                     *           
+  +-----------------+                   +-----------------+  
+  | StrOutputParser |                   | StrOutputParser |  
+  +-----------------+                   +-----------------+  
+                   ****               ****                   
+                       ***         ***                       
+                          **     **                          
+            +---------------------------------+              
+            | Parallel<chinese,english>Output |              
+            +---------------------------------+  
+"""

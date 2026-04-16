55b1940da918b4983a14930455ab679fe1eca042
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-16 19:43:22 +0800

    Update splitter.py

--- RAG/textsplit/splitter.py
+++ RAG/textsplit/splitter.py
@@ -10,3 +10,29 @@ LangChain提供多种文本分割器（Text Splitters）：将长文本拆分为
 
 3. 字符分割器 CharacterTextSplitter ： 按指定字符切分
 """
+
+
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_unstructured import UnstructuredLoader
+
+"""
+split_text()方法进行文本分割
+RecursiveCharacterTextSplitter中指定：
+- chunk_size=100,块大小为100，
+- chunk_overlap=30, 片段重叠字符数为30，
+- length_function=len，计算长度的函数使用len，# 可选：默认为字符串长度，可自定义函数来实现按 token 数切分
+"""
+# 
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=100, chunk_overlap=30, length_function=len)
+content = (
+    "大模型RAG（检索增强生成）是一种结合生成模型与外部知识检索的技术，通过从大规模文档或数据库中检索相关信息，"
+    "辅助生成模型以提升回答的准确性和相关性。其核心流程包括用户输入查询、系统检索相关知识、"
+    "生成模型基于检索结果生成内容，并输出最终答案。RAG的优势在于能够弥补生成模型的知识盲区，"
+    "提供更准确、实时和可解释的输出，广泛应用于问答系统、内容生成、客服、教育和企业领域。"
+    "然而，其也面临依赖高质量知识库、可能的响应延迟、较高的维护成本以及数据隐私等挑战。")
+
+splitter_texts = text_splitter.split_text(content)
+
+
+
+# RecursiveCharacterTextSplitter不仅可以分割纯文本，还可以直接分割Document对象

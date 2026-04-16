d8442825ce159907eb67aa4f448adee8c84286ff
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-16 20:18:49 +0800

    Update splitter.py

--- RAG/textsplit/splitter.py
+++ RAG/textsplit/splitter.py
@@ -51,8 +51,9 @@ for splitter_document in splitter_documents:
 文档片段大小：85,文档内容：区，提供更准确、实时和可解释的输出，广泛应用于问答系统、内容生成、客服、教育和企业领域。然而，其也面临依赖高质量知识库、可能的响应延迟、较高的维护成本以及数据隐私等挑战。
 """
 
-splitter_texts_forcheck = text_splitter.split_text(content)
-splitter_documents = [Document(page_content=text) for text in splitter_texts_forcheck]
+# splitter_documents = [Document(page_content=text) for text in splitter_texts]
+# for idx, splitter_document in enumerate(splitter_documents, 1):
+#     print(f"第{idx}个文档 - 大小：{len(splitter_document.page_content)}, 内容：{splitter_document.page_content}\n")
 full_content = ""
 for text in splitter_texts:
     if full_content:
@@ -64,7 +65,13 @@ print(f"拼接后文本大小：{len(full_content)}")
 print(f"是否与原始文本完全一致：{full_content == content}")
 print(f"拼接后完整内容：\n{full_content}")
 
-
-
+"""
+拼接后文本大小：225
+是否与原始文本完全一致：True
+拼接后完整内容：
+大模型RAG（检索增强生成）是一种结合生成模型与外部知识检索的技术，通过从大规模文档或数据库中检索相关信息，辅助生成模型以提升回答的准确性和相关性。其核心流程包括用
+户输入查询、系统检索相关知识、生成模型基于检索结果生成内容，并输出最终答案。RAG的优势在于能够弥补生成模型的知识盲区，提供更准确、实时和可解释的输出，广泛应用于
+问答系统、内容生成、客服、教育和企业领域。然而，其也面临依赖高质量知识库、可能的响应延迟、较高的维护成本以及数据隐私等挑战。
+"""
 
 # RecursiveCharacterTextSplitter不仅可以分割纯文本，还可以直接分割Document对象

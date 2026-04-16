ffe2509d19cd15427e8edc2b1151f8ff406083f2
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-16 16:19:38 +0800

    Update RagLoadDoc.py

--- RAG/Docloads/RagLoadDoc.py
+++ RAG/Docloads/RagLoadDoc.py
@@ -3,10 +3,23 @@ Each document loader may define its own parameters, but they share a common API:
 - load() – Loads all documents at once.
 - lazy_load() – Streams documents lazily, useful for large datasets.
 """
+# pip install langchain_community pypdf
 from langchain_community.document_loaders import TextLoader
+from langchain_community.document_loaders import PyPDFLoader
 
 file_path = "assets/sample.txt"  # 文件路径
 encoding = "utf-8"  # 文件编码方式
 
-docs = TextLoader(file_path, encoding).load()
-print(docs) # [Document(metadata={'source': 'assets/sample.txt'}, page_content='LangChain 是一个用于构建...')]
+txtdocs = TextLoader(file_path, encoding).load()
+print(txtdocs) # [Document(metadata={'source': 'assets/sample.txt'}, page_content='LangChain 是一个用于构建...')]
+
+
+
+pdfdocs = PyPDFLoader(
+    # 文件路径，本地文件或在线文件链接，如"https://arxiv.org/pdf/alg-geom/9202012"
+    file_path="assets/sample.pdf", 
+    #  提取模式: plain 提取文本 layout 按布局提取
+    extraction_mode="plain",
+).load()
+print(pdfdocs)
+

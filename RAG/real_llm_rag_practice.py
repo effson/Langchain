66f52c2260ce30a72bfb88a6f6c6ab48f2ec7510
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-16 22:33:40 +0800

    Update real_llm_rag_practice.py

--- RAG/real_llm_rag_practice.py
+++ RAG/real_llm_rag_practice.py
@@ -11,3 +11,32 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+model = init_chat_model(
+    model="qwen3.5-flash",
+    model_provider="openai",
+    api_key=os.getenv("aliQwen-api"),
+    base_url="https://dashscope.aliyuncs.com/compatible-mode/v1"
+)
+
+prompt_template = """
+    请使用以下提供的文本内容来回答问题。仅使用提供的文本信息，
+    如果文本中没有相关信息，请回答"抱歉，提供的文本中没有这个信息"。
+    文本内容：
+    {context}
+    问题：{question}
+    回答：
+    "
+"""
+
+prompt = PromptTemplate(
+    template=prompt_template,
+    input_variables=["context", "question"]
+)
+
+embeddingsModel = DashScopeEmbeddings(
+    model="text-embedding-v3", 
+    dashscope_api_key=os.getenv("aliQwen-api")
+)
+
+loader = Docx2txtLoader("alibaba-java.docx")  
+documents = loader.load()

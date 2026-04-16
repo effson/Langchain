b32bb8bb6c4a82897d1f0dbc04bf8cd0c12d9418
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-16 16:34:07 +0800

    Update RagLoadDoc.py

--- RAG/Docloads/RagLoadDoc.py
+++ RAG/Docloads/RagLoadDoc.py
@@ -7,6 +7,7 @@ Each document loader may define its own parameters, but they share a common API:
 from langchain_community.document_loaders import TextLoader
 from langchain_community.document_loaders import PyPDFLoader
 
+# TextLoader
 file_path = "assets/sample.txt"  # 文件路径
 encoding = "utf-8"  # 文件编码方式
 
@@ -14,7 +15,7 @@ txtdocs = TextLoader(file_path, encoding).load()
 print(txtdocs) # [Document(metadata={'source': 'assets/sample.txt'}, page_content='LangChain 是一个用于构建...')]
 
 
-
+# PyPDFLoader
 pdfdocs = PyPDFLoader(
     # 文件路径，本地文件或在线文件链接，如"https://arxiv.org/pdf/alg-geom/9202012"
     file_path="assets/sample.pdf", 

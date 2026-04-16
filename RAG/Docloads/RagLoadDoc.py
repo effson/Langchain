70261f6804a1fd0b9fc506d57d38e3edf9722b07
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-16 16:03:34 +0800

    Update RagLoadDoc.py

--- RAG/Docloads/RagLoadDoc.py
+++ RAG/Docloads/RagLoadDoc.py
@@ -1,9 +1,12 @@
+"""
+Each document loader may define its own parameters, but they share a common API:
+- load() – Loads all documents at once.
+- lazy_load() – Streams documents lazily, useful for large datasets.
+"""
 from langchain_community.document_loaders import TextLoader
 
-# 返回List[Document]
 file_path = "assets/sample.txt"  # 文件路径
 encoding = "utf-8"  # 文件编码方式
 
 docs = TextLoader(file_path, encoding).load()
-
 print(docs) # [Document(metadata={'source': 'assets/sample.txt'}, page_content='LangChain 是一个用于构建...')]

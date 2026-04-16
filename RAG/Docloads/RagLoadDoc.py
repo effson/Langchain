d088edc0702656c762541671c9295cc8c821fc3a
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-16 16:48:16 +0800

    Update RagLoadDoc.py

--- RAG/Docloads/RagLoadDoc.py
+++ RAG/Docloads/RagLoadDoc.py
@@ -40,7 +40,6 @@ print(pdfdocs)
 """
 
 mddocs = UnstructuredMarkdownLoader(
-    # 文件路径
     file_path="assets/sample.md",
     # 加载模式: single 返回单个Document对象，  elements 按标题等元素切分文档
     mode="elements",

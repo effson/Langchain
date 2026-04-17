41eab134aa5204ca609d90c0ba4b30f9add8f66a
Sun Jianfei <113086278+effson@users.noreply.github.com>
2026-04-17 11:04:13 +0800

    Update pgvector_llm_rag_test_query.py

--- RAG/pgvector_llm_rag_test_query.py
+++ RAG/pgvector_llm_rag_test_query.py
@@ -1,9 +1,7 @@
 import os
 
 from langchain.chat_models  import  init_chat_model
-from langchain_community.document_loaders import Docx2txtLoader
 from langchain_core.prompts import PromptTemplate
-from langchain_classic.text_splitter import CharacterTextSplitter
 from langchain_core.runnables import RunnablePassthrough
 from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_postgres import PGVector
